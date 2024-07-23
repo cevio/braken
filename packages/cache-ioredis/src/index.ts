619d44f16736e55281961444d5e27494a35e478e
沈赟杰 <shenyunjie@znrank.com>
2024-07-23 18:01:38 +0800

    update

--- packages/cache-ioredis/src/index.ts
+++ packages/cache-ioredis/src/index.ts
@@ -1,18 +1,28 @@
 import IORedis from '@braken/ioredis';
 import { CacheProps } from '@braken/cache';
-import { Application } from '@braken/application';
+import { Application, ApplicationConfigs } from '@braken/application';
 import { formatPathKey } from './utils';
 
 @Application.Injectable
 export default class IORedisCache extends Application implements CacheProps {
+  private prefix: string;
+  static readonly namespace = Symbol('prefix');
+  static prefix(value: string) {
+    ApplicationConfigs.set(IORedisCache.namespace, value);
+  }
 
   @Application.Inject(IORedis)
   private readonly redis: IORedis;
 
-  public initialize() { }
+  public initialize() {
+    if (!ApplicationConfigs.has(IORedisCache.namespace)) {
+      throw new Error('Missing namespace');
+    }
+    this.prefix = ApplicationConfigs.get(IORedisCache.namespace);
+  }
 
   public async write(key: string, value: any, time: number = 0) {
-    const path = formatPathKey(key);
+    const path = formatPathKey(key, this.prefix);
     if (!time) {
       await this.redis.connection.set(path, JSON.stringify(value));
     } else {
@@ -22,24 +32,24 @@ export default class IORedisCache extends Application implements CacheProps {
   }
 
   public async read(key: string) {
-    const path = formatPathKey(key);
+    const path = formatPathKey(key, this.prefix);
     const text = await this.redis.connection.get(path);
     return JSON.parse(text);
   }
 
   public async expire(key: string) {
-    const path = formatPathKey(key);
+    const path = formatPathKey(key, this.prefix);
     const sec = await this.redis.connection.ttl(path);
     return sec * 1000 + Date.now();
   }
 
   public async delete(key: string) {
-    const path = formatPathKey(key);
+    const path = formatPathKey(key, this.prefix);
     await this.redis.connection.del(path);
   }
 
   public async has(key: string) {
-    const path = formatPathKey(key);
+    const path = formatPathKey(key, this.prefix);
     return Boolean(await this.redis.connection.exists(path));
   }
 }
--- packages/cache-ioredis/src/utils.ts
+++ packages/cache-ioredis/src/utils.ts
@@ -1,5 +1,5 @@
-export function formatPathKey(key: string) {
-  const path = key.replace(/\//g, ':');
+export function formatPathKey(key: string, prefix: string = '') {
+  const path = (prefix + key).replace(/\//g, ':');
   if (path.startsWith(':')) return path.substring(1);
   return path;
 }
