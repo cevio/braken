aac357be2392431a4362514efc3fe2e84ec7726d
沈赟杰 <shenyunjie@znrank.com>
2024-07-29 17:10:30 +0800

    update

--- packages/http-plugin-typeorm/src/index.ts
+++ packages/http-plugin-typeorm/src/index.ts
@@ -1,5 +1,5 @@
-import { HttpTypeormPlugin, Connection } from './plugin';
+import { HttpTypeormPlugin, Connection, DataBaseTransactionWare, DataBaseWare } from './plugin';
 
 export * from './service';
-export { Connection };
+export { Connection, DataBaseTransactionWare, DataBaseWare };
 export default HttpTypeormPlugin
