8e126c1546bfbd9d70a771e0dbe1d1d837ab384c
沈赟杰 <shenyunjie@znrank.com>
2024-07-29 17:08:22 +0800

    update

--- packages/http-plugin-typeorm/src/plugin.ts
+++ packages/http-plugin-typeorm/src/plugin.ts
@@ -1,5 +1,5 @@
 import TypeORM from '@braken/typeorm';
-import { Plugin } from '@braken/http';
+import { Middleware, Plugin } from '@braken/http';
 import { Context, Next } from 'koa';
 import { DataSource, QueryRunner } from 'typeorm';
 
@@ -26,21 +26,27 @@ export class HttpTypeormPlugin extends Plugin {
 
   public onRequest() { }
   public onResponse() { }
+}
 
-  static Middleware(useable?: boolean) {
-    return async (ctx: Context, next: Next) => {
-      const typeorm = await ctx.$module.use(TypeORM);
-      if (useable) {
-        await typeorm.transaction(async (runner, roll) => {
-          ctx.$typeorm_connection = runner;
-          ctx.$typeorm_transacation_rollback = roll;
-          return await next();
-        })
-      } else {
-        ctx.$typeorm_connection = typeorm.connection;
-        await next();
-      }
-    }
+@Middleware.Injectable
+export class DataBaseWare extends Middleware {
+  @Middleware.Inject(TypeORM)
+  private readonly typeorm: TypeORM;
+  public async use(ctx: Context, next: Next) {
+    ctx.$typeorm_connection = this.typeorm.connection;
+    await next();
   }
 }
 
+@Middleware.Injectable
+export class DataBaseTransactionWare extends Middleware {
+  @Middleware.Inject(TypeORM)
+  private readonly typeorm: TypeORM;
+  public async use(ctx: Context, next: Next) {
+    await this.typeorm.transaction(async (runner, roll) => {
+      ctx.$typeorm_connection = runner;
+      ctx.$typeorm_transacation_rollback = roll;
+      return await next();
+    })
+  }
+}
