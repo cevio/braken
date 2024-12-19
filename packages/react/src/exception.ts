bf42a07a813f4837a760d59076854b5f98a0a06f
沈赟杰 <shenyunjie@znrank.com>
2024-12-19 14:14:00 +0800

    update

--- packages/react/src/exception.ts
+++ packages/react/src/exception.ts
@@ -2,7 +2,6 @@ export class Exception extends Error {
   public readonly status: number;
   public readonly timestamp = Date.now();
 
-
   constructor(status: number, msg?: string) {
     super(msg);
     this.status = status;
